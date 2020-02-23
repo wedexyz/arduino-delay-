31cf4063720d35afd8c2aeb25c4b97e5a65d1fdf
wedexyz <38834987+wedexyz@users.noreply.github.com>
2020-02-23 16:51:01 +0700

    Add files via upload

---
+++ delaydenganfungsi_milis.ino
@@ -0,0 +1,19 @@
+
+unsigned long interval=1000; 
+unsigned long previousMillis=0; 
+bool ledState = false; 
+  
+void setup() {
+ pinMode( LED_BUILTIN, OUTPUT);
+ digitalWrite(13, ledState);
+}
+  
+void loop() {
+ unsigned long currentMillis = millis(); 
+  
+ if ((unsigned long)(currentMillis - previousMillis) >= interval) {
+   ledState = !ledState; 
+   digitalWrite( LED_BUILTIN, ledState); 
+   previousMillis = millis();
+ }
+}
